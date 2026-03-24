f027b8cd84213545837d3523150bad4364b30b91
pplayer1712 <p.player1712.ad@gmail.com>
2026-03-24 09:39:45 +0100

    first commit

---
+++ src/main/kotlin/ase_filament_tracker/AseFilamentTrackerApplication.kt
@@ -0,0 +1,11 @@
+package ase_filament_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AseFilamentTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<AseFilamentTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/ase_filament_tracker/AseFilamentTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ase_filament_tracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AseFilamentTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
