571dd1c476d32d7cd7353fe8fb93f35484b390f4
pplayer1712 <p.player1712.ad@gmail.com>
2026-05-30 19:16:37 +0200

    reorder plugin declarations in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
 	kotlin("jvm") version "2.2.21"
 	kotlin("plugin.spring") version "2.2.21"
-	id("org.springframework.boot") version "4.0.4"
-	id("io.spring.dependency-management") version "1.1.7"
+        kotlin("plugin.jpa") version "2.2.21"
+        id("org.springframework.boot") version "4.0.4"
+        id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.example"
